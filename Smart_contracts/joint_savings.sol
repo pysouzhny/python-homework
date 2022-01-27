441366f6d1d8fb0efd3b86b4d5ed2cd9a855365e
pysouth dj <mrngondji@gmail.com>
2022-01-26 21:26:54 -0500

    contract

---
+++ Smart_contracts/joint_savings.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+import "hardhat/console.sol";
+
+contract JointSavings{
+
+    address payable accountOne;
+    address payable accountTwo;
+    address payable lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+    
+    constructor () public payable { }
+
+    function withdraw(uint amount, address payable recipient) public {
+        require(recipient == accountOne || recipient == accountTwo, "You don’t own this account!");
+        require(address(this).balance >= amount, "Insufficient funds!");
+        if(lastToWithdraw != recipient){
+            lastToWithdraw = recipient;
+        }
+        recipient.transfer(amount);
+        lastWithdrawAmount = amount;
+        contractBalance = address(this).balance;
+    }
+
+    function deposit() public payable {
+        contractBalance = address(this).balance;
+    }
+
+    function setAccounts( address payable account1, address payable account2) public{
+        accountOne = account1;
+        accountTwo = account2;
+    
+    }
+    function() external payable {
+    }
+}
